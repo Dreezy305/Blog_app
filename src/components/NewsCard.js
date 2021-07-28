74ba6249cce806aefca491ae41e9ba3408ed6113
Dreezy305 <bankoleidris@gmail.com>
2021-07-28 14:09:09 +0000

    finshed working on news card

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -5,7 +5,24 @@ function NewsCard() {
   return (
     <>
       <div className="row">
-        <div>Working on New NewsCard</div>
+        <div className="leftcolumn">
+          <div className="card">
+            <h2>TITLE HEADING</h2>
+            <h5>Title description, Dec 7, 2017</h5>
+            <div className="fakeimg" style={{ height: "200px" }}>
+              Image
+            </div>
+            <p>Some text..</p>
+          </div>
+          <div className="card">
+            <h2>TITLE HEADING</h2>
+            <h5>Title description, Sep 2, 2017</h5>
+            <div className="fakeimg" style={{ height: "200px" }}>
+              Image
+            </div>
+            <p>Some text..</p>
+          </div>
+        </div>
       </div>
     </>
   );
