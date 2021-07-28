528303cc34e051b0f53e922b4509b158df38a8c1
Dreezy305 <bankoleidris@gmail.com>
2021-07-28 14:27:04 +0000

    working on news comment

--- src/components/NewsComment.js
+++ src/components/NewsComment.js
@@ -1,7 +1,29 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 
 function NewsComment() {
-  return <div></div>;
+  return (
+    <>
+      <div className="form-floating w-100">
+        <textarea
+          className="form-control"
+          placeholder="Leave a comment here"
+          id="floatingTextarea2"
+          style={{ height: "100px" }}
+        ></textarea>
+        <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+          <button className="btn btn-primary me-md-2" type="button">
+            Post Comment
+          </button>
+          <button className="btn btn-primary" type="button">
+            Delete
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default NewsComment;
